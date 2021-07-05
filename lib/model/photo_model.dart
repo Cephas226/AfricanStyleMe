8fa4a3ff24b9c5a7f4c177984cdafeab411e146d
CoolyDev <cephaszoubga@gmail.com>
2021-07-06 00:05:22 +0100

    ok

---
+++ lib/model/photo_model.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+
+class Dataservices {
+  static var client = http.Client();
+
+  static Future<List<Photo>> getTodo() async {
+    var response =
+    await client.get('https://jsonplaceholder.typicode.com/todos');
+
+    if (response.statusCode == 200) {
+      var data = response.body;
+      return todoFromJson(data);
+    } else {
+      return null;
+    }
+  }
+}
